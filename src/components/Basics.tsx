22c2cc48de2cfbad1367fe69112175d487a7617b
nstylo <niklas@stylianou.info>
2019-12-04 00:35:15 +0100

    Button now uses props.theme

--- src/components/Basics.tsx
+++ src/components/Basics.tsx
@@ -1,15 +1,12 @@
 import styled from "styled-components"
 
 export const Button = styled.button`
+  font-size: 0.5em;
   background-color: transparent;
-  color: #006990;
+  color: ${(props): string => props.theme.primary};
   border: solid;
   border-width: 3px;
-  border-color: #006990;
+  border-color: ${(props): string => props.theme.primary};
   border-radius: 5px;
   padding: 0.25em 1em;
-`
-
-export const H1 = styled.h1`
-  margin: 0;
 `
