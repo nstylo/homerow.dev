83b8d984b46e061202a1beeb566680a60f38c342
nstylo <niklas@stylianou.info>
2019-12-04 00:34:40 +0100

    Update NavBar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,32 +2,70 @@ import { Link } from "gatsby"
 import styled from "styled-components"
 import React from "react"
 
-import { H1 } from "./Basics"
+interface ItemProps {
+  name: string
+  path: string // path to redirect on click
+  className: string
+}
+
+const Item = ({ name, path, className }: ItemProps): JSX.Element => (
+  <Container>
+    <h3 className={className}>
+      <StyledLink to={path}>{name}</StyledLink>
+    </h3>
+  </Container>
+)
+
+interface MainItemProps {
+  name: string
+  path?: string // path is optional, default is "/"
+  className: string
+}
+
+const MainItem = ({ name, path = "/", className }: MainItemProps): JSX.Element => (
+  <Container>
+    <h2 className={className}>
+      <StyledLink to={path}>{name}</StyledLink>
+    </h2>
+  </Container>
+)
 
 interface HeaderProps {
   siteTitle: string
   className: string
 }
 
-const Header = ({ siteTitle = `No Title Given`, className }: HeaderProps): JSX.Element => (
+// const SocialBar = () => (
+//
+// )
+
+const Header = ({ siteTitle, className }: HeaderProps): JSX.Element => (
   <header className={className}>
     <HeaderWrapper>
-      <H1>
-        <HeaderLink to="/">{siteTitle}</HeaderLink>
-      </H1>
+      <MainItem name={siteTitle} className={"mainitem"} />
+      <StyledItem name="Home" path="/" className={"item"} />
+      <StyledItem name="Home" path="/" className={"item"} />
+      <StyledItem name="Home" path="/" className={"item"} />
     </HeaderWrapper>
   </header>
 )
 
-const HeaderLink = styled(Link)`
-  color: white;
-  text-decoration: none;
+const StyledItem = styled(Item)``
+
+const Container = styled.div`
+  flex-grow: 1;
 `
 
 const HeaderWrapper = styled.div`
+  display: flex;
   margin: 0 auto;
-  max-width: 960;
-  padding: 1.45rem 1.0875rem;
+  max-width: 1024px;
+  padding: 0.5rem 1rem;
+`
+
+const StyledLink = styled(Link)`
+  color: ${(props): string => props.theme.foreground};
+  text-decoration: none;
 `
 
 const StyledHeader = styled(Header)`
