7e39c08ef546811c7ac3d0422921570eb6a7a3ec
nstylo <niklas@stylianou.info>
2019-12-05 15:28:21 +0100

    Add socialbar with svg links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
 import { Link } from "gatsby"
 import styled from "styled-components"
 import React from "react"
+import LinkedInSvg from "../images/linkedin.svg"
+import RssSvg from "../images/rss.svg"
 
 interface ItemProps {
   name: string
@@ -30,22 +32,45 @@ const MainItem = ({ name, path = "/", className }: MainItemProps): JSX.Element =
   </Container>
 )
 
+interface ImageLinkProps {
+  path?: string
+  imgSource: string
+  alt: string
+  className: string
+}
+
+const ImageLink = ({ path = "/", imgSource, alt, className }: ImageLinkProps): JSX.Element => (
+  <Link to={path} className={className}>
+    <img src={imgSource} alt={alt} />
+  </Link>
+)
+
+const StyledImageLink = styled(ImageLink)`
+  height: 100%;
+  width: auto;
+  img {
+    height: 100%;
+    width: 40px;
+    margin: 0 10px;
+  }
+`
+
 interface HeaderProps {
   siteTitle: string
   className: string
 }
-
-// const SocialBar = () => (
-//
-// )
 
 const Header = ({ siteTitle, className }: HeaderProps): JSX.Element => (
   <header className={className}>
     <HeaderWrapper>
       <MainItem name={siteTitle} className={"mainitem"} />
       <StyledItem name="Home" path="/" className={"item"} />
-      <StyledItem name="Home" path="/" className={"item"} />
-      <StyledItem name="Home" path="/" className={"item"} />
+      <StyledItem name="Projects" path="/projects" className={"item"} />
+      <StyledItem name="Blog" path="/blog" className={"item"} />
+      <Container>
+        <StyledImageLink path="/test" imgSource={LinkedInSvg} alt="LinkedIn" className="imagelink" />
+        <StyledImageLink path="/test" imgSource={RssSvg} alt="RSS" className="imagelink" />
+      </Container>
     </HeaderWrapper>
   </header>
 )
@@ -53,14 +78,19 @@ const Header = ({ siteTitle, className }: HeaderProps): JSX.Element => (
 const StyledItem = styled(Item)``
 
 const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   flex-grow: 1;
 `
 
 const HeaderWrapper = styled.div`
   display: flex;
   margin: 0 auto;
-  max-width: 1024px;
-  padding: 0.5rem 1rem;
+  justify-content: center;
+  max-width: 1280px;
+  max-height: 40px;
+  padding: 0.5rem 0.5rem;
 `
 
 const StyledLink = styled(Link)`
