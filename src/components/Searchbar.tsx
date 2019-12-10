8a3775864cc7c77dfd34bc93698f948cb481bb41
nstylo <niklas@stylianou.info>
2019-12-10 13:38:29 +0100

    Add Searchbar

---
+++ src/components/Searchbar.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react"
+import styled from "styled-components"
+
+interface SearchbarProps {
+  className: string
+}
+
+const Searchbar = ({ className }: SearchbarProps): JSX.Element => {
+  const [value, setValue] = useState("")
+
+  return <input type="text" value={value} onChange={(e): void => setValue(e.target.value)} className={className} />
+}
+
+const StyledSearchbar = styled(Searchbar)``
+
+export default StyledSearchbar
