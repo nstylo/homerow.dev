bd0f58925a6cb470a44ff86d3627f1badfaecabe
nstylo <niklas@stylianou.info>
2019-12-14 17:15:53 +0100

    Add minimal projects page

---
+++ src/pages/projects.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import styled from "styled-components"
+
+import Layout from "../components/Layout"
+
+const Projects = ({ children }) => <Layout>{children}</Layout>
+
+export default Projects
