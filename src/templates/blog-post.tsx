730e778bb16b3a7442c870d0342bdad31996c9d8
nstylo <niklas@stylianou.info>
2020-02-03 00:02:09 +0100

    Define Article component for blog-post and change link color

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -1,26 +1,40 @@
 import React from "react"
 import { graphql } from "gatsby"
-import Layout from "../components/Layout"
+import styled from "styled-components"
 
+import Layout from "../components/Layout"
 import SubscriptionForm from "../forms/SubscriptionForm"
 import Comments from "../components/Comments"
 
-class BlogPost extends React.Component {
+class BlogPost extends React.Component<{ className: string }> {
   render(): JSX.Element {
     const post = this.props.data.markdownRemark
 
     return (
       <Layout>
-        <div>
-          <h2>{post.frontmatter.title}</h2>
-          <div dangerouslySetInnerHTML={{ __html: post.html }} />
-          <Comments className="comments" />
-          <SubscriptionForm />
-        </div>
+        <StyledArticle post={post} className="blog" />
+        <Comments className="comments" />
+        <SubscriptionForm />
       </Layout>
     )
   }
 }
+
+const Article = ({ className, post }) => {
+  return (
+    <div className={className}>
+      <h2>{post.frontmatter.title}</h2>
+      <div dangerouslySetInnerHTML={{ __html: post.html }} />
+    </div>
+  )
+}
+
+const StyledArticle = styled(Article)`
+  a {
+    color: ${(props): string => props.theme.primary};
+    text-decoration: none;
+  }
+`
 
 export const query = graphql`
   query($slug: String!) {
